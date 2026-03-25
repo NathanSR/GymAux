6220012ebdafe68ae64f485f002ecf917859266c
Nathan Santos <nathansr.nsr@gmail.com>
2026-03-25 18:55:06 -0300

    sounds file fixed in proxy

--- src/proxy.ts
+++ src/proxy.ts
@@ -55,5 +55,5 @@ export default async function middleware(request: NextRequest) {
 }
 
 export const config = {
-    matcher: ['/((?!api|_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)']
+    matcher: ['/((?!api|_next/static|_next/image|sounds|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp|mp3)$).*)']
 };
